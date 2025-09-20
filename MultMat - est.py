c6278883a24e07fc34939cd364a823abdbfac9f2
Exmator <151688577+Exmator@users.noreply.github.com>
2025-09-20 13:16:14 -0500

    Add función recursiva

--- MultMat - est.py
+++ MultMat - est.py
@@ -2,8 +2,6 @@ import sys
 import numpy as np
 import time
 
-
-
 def mat_mult(A):
     init = time.perf_counter_ns()
     n = len(A)
@@ -13,6 +11,39 @@ def mat_mult(A):
     return 0
 # end def
 
+# Complejidad O(2^n)
+def mult_chain(d, i, j):
+    print(f'Calling {i},{j}')
+    if i == j:
+        return 0
+    else:
+        cost = float('inf')
+        for k in range(i, j):
+            cost_act = (mult_chain(d, i, k) +
+                        mult_chain(d, k + 1, j) +
+                        d[i - 1] * d[k] * d[j])
+            if cost_act < cost:
+                cost = cost_act
+            # end if
+        return cost
+        # end for
+    # end if
+# end def
+
+def memoizacion():
+    # TODO: Implementar memoización
+    pass
+# end def
+
+def bottom_up():
+    # TODO: Implementar enfoque de abajo hacia arriba
+    pass
+# end def
+
+def analisis_de_memoria():
+    # TODO: Implementar análisis de memoria
+    pass
+# end def
 ## -------------------------------------------------------------------------
 if len(sys.argv) < 2:
     print("Usage: python3", sys.argv[0], "input_file")
